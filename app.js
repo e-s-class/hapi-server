ed80df621c96143f394152655e31a047a963fc63
jade <jumpitup82@gmail.com>
2019-03-11 02:01:03 +0900

    initial

---
+++ app.js
@@ -0,0 +1,13 @@
+const BaseAppLoader = require('./lib/base-app-loader');
+
+class AppLoader extends BaseAppLoader {
+    constructor(props) {
+        super(props);
+    }
+
+    updateConfigDependencies() {
+
+    }
+}
+
+module.exports = AppLoader;
