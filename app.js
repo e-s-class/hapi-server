654ee4959808c35ccf900e35cb39bbbd03aad620
jade <jumpitup82@gmail.com>
2019-03-13 13:57:58 +0900

    route handler

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+const pgp = require('pg-promise')();
 const BaseAppLoader = require('./lib/base-app-loader');
+
+// Database connection details;
+const cn = {
+    host: 'cockroachdb', // 'localhost' is the default;
+    port: 26257, // 5432 is the default;
+    database: 'test_database',
+    user: 'root',
+};
 
 class AppLoader extends BaseAppLoader {
     constructor(props) {
@@ -6,7 +15,7 @@ class AppLoader extends BaseAppLoader {
     }
 
     updateConfigDependencies() {
-
+        this.dependencies.db =  pgp(cn);
     }
 }
 
